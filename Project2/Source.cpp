7ef52bcd1776d835a00b9b509ee031c1dc57e702
Denis Timonin <denis.timonin@semsotec.com>
2020-01-20 13:03:52 +0300

    add checking for user32 module

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -5,11 +5,15 @@ BOOL WINAPI DllMain(HINSTANCE app, DWORD reas, LPVOID res)
 {
     switch (reas) {
     case DLL_PROCESS_ATTACH: {
+        if(GetModuleHandleA("User32")) {
             MessageBoxA(NULL, "process attached", "DLL", MB_OK);
-            break;
         }
+            break;
+    }
         case DLL_PROCESS_DETACH: {
-            MessageBoxA(NULL, "process detached", "DLL", MB_OK);
+            if (GetModuleHandleA("User32")) {
+                MessageBoxA(NULL, "process detached", "DLL", MB_OK);
+            }
             break;
         }
         case DLL_THREAD_ATTACH:
